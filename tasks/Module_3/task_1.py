69d3fa4a88901b2415b22f459185539c242e3c2b
SmaSH1982 <azarenok.alex@gmail.com>
2020-06-22 14:02:27 +0200

    Updated task_1 due to code review comments (part1)

--- tasks/Module_3/task_1.py
+++ tasks/Module_3/task_1.py
@@ -1,38 +1,39 @@
 from collections import defaultdict
 
 import pyinputplus as pyip
-import random
 
 ingredients_and_prices = defaultdict()
 
+bread_price = {"wheat": 1.5, "white": 0.8, "sourdough": 2.1}
+meat_price = {"chicken": 5, "turkey": 3.2, "ham": 4.1, "tofu": 5.5}
+cheese_price = {"cheddar": 3.1, "swiss": 2.5, "mozzarella": 1.9}
+sauce_price = {"mayo": 0.7, "mustard": 0.2, "lettuce": 0.5, "tomato": 1.1}
+
 bread_type = pyip.inputMenu(['wheat', 'white', 'sourdough'], numbered=True)
-bread_price = round(random.uniform(3.5, 7), 2)
-ingredients_and_prices[bread_type] = bread_price
+bread_type_price = bread_price.get(bread_type)
+ingredients_and_prices[bread_type] = bread_type_price
 
-protein_type = pyip.inputMenu(['chicken', 'turkey', 'ham', 'tofu'], numbered=True)
-protein_price = round(random.uniform(2, 4.5), 2)
-ingredients_and_prices[protein_type] = protein_price
+meat_type = pyip.inputMenu(['chicken', 'turkey', 'ham', 'tofu'], numbered=True)
+meat_type_price = meat_price.get(meat_type)
+ingredients_and_prices[meat_type] = meat_type_price
 
-prompt = 'Do you want a cheese?\n'
-response = pyip.inputYesNo(prompt)
+response = pyip.inputYesNo('Do you want a cheese?\n')
 if response == 'yes':
     cheese_type = pyip.inputMenu(['cheddar', 'Swiss', 'mozzarella'], numbered=True)
-    cheese_price = round(random.uniform(5, 8.5), 2)
-    ingredients_and_prices[cheese_type] = cheese_price
+    cheese_type_price = cheese_price.get(cheese_type)
+    ingredients_and_prices[cheese_type] = cheese_type_price
 
-prompt = 'Do you want a sauce type?\n'
-response = pyip.inputYesNo(prompt)
+response = pyip.inputYesNo('Do you want a sauce type?\n')
 if response == 'yes':
     sauce_type = pyip.inputMenu(['mayo', 'mustard', 'lettuce', 'tomato'], numbered=True)
-    sauce_price = round(random.uniform(6.5, 7.5), 2)
-    ingredients_and_prices[sauce_type] = sauce_price
+    sauce_type_price = sauce_price.get(sauce_type)
+    ingredients_and_prices[sauce_type] = sauce_type_price
 
-prompt = 'How many sandwiches do you want to buy?\n'
-sandwiches_count = pyip.inputNum(prompt, min=1)
+sandwiches_count = pyip.inputNum('How many sandwiches do you want to buy?\n', min=1)
 
 total_ingredients_price = sum(ingredients_and_prices.values())
 
-print("Your sandwich consists of " + ', '.join(ingredients_and_prices.keys()) + ".")
+print(f"Your sandwich consists of {', '.join(ingredients_and_prices.keys())}.")
 print(f"You want to buy {sandwiches_count} sandwich(es)")
 print(f"That's a total of {sandwiches_count * total_ingredients_price} eur")
 
