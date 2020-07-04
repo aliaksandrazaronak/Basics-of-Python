2663cb589a1fbeba717eb0eef6434506d16cedf0
SmaSH1982 <azarenok.alex@gmail.com>
2020-07-04 13:52:59 +0200

    Fixes after code review (part2)

--- tasks/Module_4/task_2.py
+++ tasks/Module_4/task_2.py
@@ -5,12 +5,12 @@ FILE_PREFIX = "spam"
 new_list = []
 
 for file in os.listdir():
-    if file.startswith(FILE_PREFIX):
+    if file.startswith(FILE_PREFIX) and os.path.isfile(file):
         new_list.append(file)
+        sorted_list = sorted(new_list)
 
-for index, file in enumerate(new_list, start=1):
-    if str(index) not in file:
-        file_postfix = str(index).zfill(3)
-        new_file = f"{FILE_PREFIX}{file_postfix}.txt"
-        os.rename(file, new_file)
+for index, file in enumerate(sorted_list, start=1):
+    file_postfix = str(index).zfill(3)
+    new_file = f"{FILE_PREFIX}{file_postfix}.txt"
+    os.rename(file, new_file)
 print(os.listdir())
