6613da8fda069ca918fe61afeb112ca5367e585f
SmaSH1982 <azarenok.alex@gmail.com>
2020-07-04 17:16:05 +0200

    Fixes after code review (part3)

--- tasks/Module_4/task_1.py
+++ tasks/Module_4/task_1.py
@@ -1,35 +1,23 @@
 import pyinputplus as pyip
 
-ADJECTIVE = "ADJECTIVE"
-NOUN = "NOUN"
-VERB = "VERB"
+PLACEHOLDERS = ('ADJECTIVE', 'NOUN', 'VERB')
 
 
 def mad_libs(file_for_read, file_for_write):
     """Read content from file, replace words of speech with entered ones and save outcomes to new file."""
-    with open(file_for_read, "r") as rf:
-        replaced_text = ""
-        new_text = ""
+    with open(file_for_read, "r") as rf, open(file_for_write, "w") as wf:
+        new_words = []
         for line in rf:
-            for index, character in enumerate(line):
-                new_text += character
-                if new_text.find(ADJECTIVE) != -1:
-                    adjective_value = pyip.inputStr("Enter an adjective: ")
-                    replaced_text += new_text.replace(ADJECTIVE, adjective_value)
-                    new_text = ""
-                if new_text.find(NOUN) != -1:
-                    noun_value = pyip.inputStr("Enter a noun: ")
-                    replaced_text += new_text.replace(NOUN, noun_value)
-                    new_text = ""
-                if new_text.find(VERB) != -1:
-                    verb_value = pyip.inputStr("Enter a verb: ")
-                    replaced_text += new_text.replace(VERB, verb_value)
-                    new_text = ""
-        final_text = replaced_text + new_text
-
-        with open(file_for_write, "w") as wf:
-            wf.write(final_text)
-        print(final_text)
+            for word in line.split(" "):
+                if word in PLACEHOLDERS:
+                    new_value = pyip.inputStr(f"Enter {word.lower()}: ")
+                    new_words.append(new_value)
+                elif word.split(".")[0] in PLACEHOLDERS:
+                    new_value = pyip.inputStr(f"Enter {word.split('.')[0].lower()}: ")
+                    new_words.append(f"{new_value}.")
+                else:
+                    new_words.append(word)
+        wf.write(" ".join(new_words))
 
 
 mad_libs("task_1.txt", "new_task_1.txt")
